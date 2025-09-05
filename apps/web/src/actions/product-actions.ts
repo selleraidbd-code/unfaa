1ec9dbdad9bbd4eb70537181226af28fe09ec702
masud-rana9 <rana61960@gmail.com>
2025-09-05 19:06:37 +0600

    Refactor product actions and pages: Update getProducts function to include categoryId, enhance ProductsPage to accept products as props, and implement category page to fetch products based on category ID. Improve HomeCategories and ProductCard components for better layout and functionality.

--- apps/web/src/actions/product-actions.ts
+++ apps/web/src/actions/product-actions.ts
@@ -1,32 +1,37 @@
 "use server";
 
-import { ResponseObject } from "@/types";
+import { PaginatedResponse, ResponseObject } from "@/types";
 
 import { CustomFetch } from "@/lib/CustomFetch";
 import { Product } from "@/types/product-type";
 
-// export const getProducts = async ({ page, limit }: { page?: string; limit?: number }) => {
-//     const url = `/product?shopId=${config.shopId}&limit=${limit || 20}${page ? `&page=${page}` : ""}`;
-//     console.log("called getProducts", url);
-//     const products = await CustomFetch<PaginatedResponse<Product>>(url);
+export const getProducts = async ({
+  page,
+  limit,
+}: {
+  page?: string;
+  limit?: number;
+  categoryId?: string;
+}) => {
+  const url = `/product?limit=${limit || 20}${page ? `&page=${page}` : ""}`;
+  console.log("called getProducts", url);
+  const products = await CustomFetch<PaginatedResponse<Product>>(url);
 
-//     console.log("products", products);
+  console.log("products", products);
 
-//     return products;
-// };
+  return products;
+};
 
 export const getProductById = async (id: string) => {
-    const product = await CustomFetch<ResponseObject<Product>>(
-        `/product/${id}`
-    );
-    return product;
+  const product = await CustomFetch<ResponseObject<Product>>(`/product/${id}`);
+  return product;
 };
 
 export const getProductBySlug = async (shopSlug: string, slug: string) => {
-    const url = `/product/product-details/${shopSlug}/${slug}`;
-    console.log("getProductBySlug called", url);
-    const product = await CustomFetch<ResponseObject<Product>>(url);
-    return product;
+  const url = `/product/product-details/${shopSlug}/${slug}`;
+  console.log("getProductBySlug called", url);
+  const product = await CustomFetch<ResponseObject<Product>>(url);
+  return product;
 };
 
 // export const getAllProducts = async (): Promise<PaginatedResponse<Product>> => {
--- apps/web/src/features/products/ProductsPage.tsx
+++ apps/web/src/features/products/ProductsPage.tsx
@@ -16,118 +16,16 @@ import {
   DropdownMenuTrigger,
 } from "@workspace/ui/components/dropdown-menu";
 import { Button } from "@workspace/ui/components/button";
-import { ProductCard } from "@/features/products/ProductCard";
-import { useState } from "react";
 
-export const ProductsPage = () => {
-  const products = [
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744147422894-670303793.jpg",
-      discount: "-33%",
-      category: "Wellness & Self-Care",
-      title: "Simplus 3D Floating Plate Hair Straightener - Type-C...",
-      currentPrice: 2000,
-      originalPrice: 3000,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744566237331-957292394.jpg",
-      category: "Haircare",
-      title: "Feature Hair Care Collection – Complete Nourishment for Every...",
-      currentPrice: 1500,
-      originalPrice: 2000,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1745580373811-76572101.jpg",
-      category: "Beauty Tools & Accessories",
-      title: "Rakibul Hasan",
-      currentPrice: 1500,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744147422894-670303793.jpg",
-      discount: "-33%",
-      category: "Men's Grooming",
-      title:
-        "Top-Selling Makeup Primers Collection - Best for Matte Finish,...",
-      currentPrice: 800,
-      originalPrice: 1200,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744147422894-670303793.jpg",
-      discount: "-38%",
-      category: "Skincare",
-      title: "Heng Fang Dream Matte Lipstick Set - 6-Piece Velvet Smooth...",
-      currentPrice: 500,
-      originalPrice: 800,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744627430617-339961119.jpg",
-      discount: "-38%",
-      category: "Wellness & Self-Care",
-      title: "Rael Beauty Miracle Clear Exfoliating Cleanser - Gentle &...",
-      currentPrice: 500,
-      originalPrice: 800,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744147422894-670303793.jpg",
-      discount: "-29%",
-      category: "Beauty Tools & Accessories",
-      title: "Giorgio Armani – Acqua di Gioia Eau de Parfum (EDP) for Women",
-      currentPrice: 500,
-      originalPrice: 700,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744566154755-14651985.jpg",
-      discount: "-29%",
-      category: "Bath & Body",
-      title: "Makeup Blender Set in Decorative Flower Jar - 4-Piece Beauty...",
-      currentPrice: 500,
-      originalPrice: 700,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744147422894-670303793.jpg",
-      discount: "-29%",
-      category: "Beauty Tools & Accessories",
-      title: "Giorgio Armani – Acqua di Gioia Eau de Parfum (EDP) for Women",
-      currentPrice: 500,
-      originalPrice: 700,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744566154755-14651985.jpg",
-      discount: "-29%",
-      category: "Bath & Body",
-      title: "Makeup Blender Set in Decorative Flower Jar - 4-Piece Beauty...",
-      currentPrice: 500,
-      originalPrice: 700,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744147422894-670303793.jpg",
-      discount: "-29%",
-      category: "Beauty Tools & Accessories",
-      title: "Giorgio Armani – Acqua di Gioia Eau de Parfum (EDP) for Women",
-      currentPrice: 500,
-      originalPrice: 700,
-    },
-    {
-      imageSrc:
-        "http://multi-media-server.naimurrhman.com/uploads/img/1744566154755-14651985.jpg",
-      discount: "-29%",
-      category: "Bath & Body",
-      title: "Makeup Blender Set in Decorative Flower Jar - 4-Piece Beauty...",
-      currentPrice: 500,
-      originalPrice: 700,
-    },
-  ];
+import { useState } from "react";
+import { Product } from "@/types/product-type";
+import { ProductCard } from "@/features/home-page/product-card";
+
+type ProductsPageProps = {
+  products: Product[];
+};
+export const ProductsPage = ({ products }: ProductsPageProps) => {
+  console.log("products :>> ", products);
 
   const categories = [
     "Makeup",
@@ -168,7 +66,7 @@ export const ProductsPage = () => {
     setSelectedSort("");
   };
   return (
-    <div className="container mx-auto px-4 py-6 md:px-6 lg:px-8">
+    <div className="container mx-auto px-4 py-6 ">
       <div className="flex items-center gap-2 text-sm text-gray-500 mb-6">
         <HomeIcon className="w-4 h-4" />
         <Link href="#" className="hover:underline">
@@ -183,13 +81,13 @@ export const ProductsPage = () => {
         <aside className="space-y-6">
           <Accordion type="multiple" className="" defaultValue={["category"]}>
             <AccordionItem value="category">
-              <AccordionTrigger className="text-base font-semibold px-4 py-2">
+              <AccordionTrigger className="text-base font-semibold  py-1">
                 Category
               </AccordionTrigger>
-              <AccordionContent className="px-4 pb-4 grid gap-4 border-b border-gray-200">
+              <AccordionContent className=" pb-4 grid gap-4 border-b border-muted-foreground mt-2">
                 {categories.map((category) => (
                   <div key={category} className="flex items-center space-x-2">
-                    <Checkbox id={category} />
+                    <Checkbox className="text-muted-foreground" id={category} />
                     <label
                       htmlFor={category}
                       className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -204,10 +102,10 @@ export const ProductsPage = () => {
 
           <Accordion type="multiple" defaultValue={["size"]}>
             <AccordionItem value="size">
-              <AccordionTrigger className="text-base font-semibold px-4 py-2">
+              <AccordionTrigger className="text-base font-semibold py-1">
                 Size
               </AccordionTrigger>
-              <AccordionContent className="px-4 pb-4 grid gap-4 border-b border-gray-200">
+              <AccordionContent className=" pb-4 grid gap-4 border-b border-muted-foreground mt-2">
                 {sizes.map((size) => (
                   <div key={size} className="flex items-center space-x-2">
                     <Checkbox id={size} />
@@ -225,10 +123,10 @@ export const ProductsPage = () => {
 
           <Accordion type="multiple" defaultValue={["color"]}>
             <AccordionItem value="color">
-              <AccordionTrigger className="text-base font-semibold px-4 py-2 ">
+              <AccordionTrigger className="text-base font-semibold  py-2 ">
                 Color
               </AccordionTrigger>
-              <AccordionContent className="px-4 pb-4 grid gap-4 border-b border-gray-200">
+              <AccordionContent className=" pb-4 grid gap-4 border-b border-muted-foreground mt-2">
                 {colors.map((color) => (
                   <div key={color} className="flex items-center space-x-2">
                     <Checkbox id={color} />
@@ -295,7 +193,7 @@ export const ProductsPage = () => {
           <Link href={"/products/id"}>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-4">
               {products.map((product, index) => (
-                <ProductCard key={index} {...product} />
+                <ProductCard key={index} product={product} />
               ))}
             </div>
           </Link>
